bf30b57131e691729fd4530a5b9c271468470572
Murilo Lima <83564133+Lima-Murilo@users.noreply.github.com>
2021-08-19 13:29:38 +0000

    Exercícios 1-7

---
+++ index.js
@@ -0,0 +1,51 @@
+// Exercicio 1
+  console.log("O primeiro programa a gente nunca esquece!")
+  console.log("__________________________________________")
+
+//Exercicio 2
+  // const nomeCompleto = prompt("Digite seu nome completo")
+  // const endereco = prompt("Digite seu endereço")
+  // const cepTelefone = prompt("Digite seu Cep e número de telefone")
+  console.log("Murilo Lima Carrer")
+  console.log("Rua Gonzaguinha n 51 apto 13")
+  console.log("09271-255 e 11992571297")
+  console.log("__________________________________________")
+
+//Exercicio 3
+  console.log("Soon may the Wellerman come \nTo bring us sugar and tea and rum \nOne day, when the tonguing is done \nWe'll take our leave and go")
+  console.log("__________________________________________")
+
+// Exercicio 4
+  console.log("Aluno (a)    Nota")
+  console.log("=========    ====")
+  console.log("Aline        9.0")
+  console.log("Mário        Dez")
+  console.log("Sérgio       4.5")
+  console.log("Shirley      7.0")
+  console.log("__________________________________________")
+
+// Exercicio 5
+  console.log("Cadastro de Clientes \n0 – Fim \n1 - Inclui \n2 - Altera \n3 - Exclui \n4 - Consulta")
+  // let opcao = prompt("Escolha uma opção")
+  // console.log(`A opção escolhida foi a: ${ opcao }`)
+  console.log("__________________________________________")
+
+// Exercicio 6
+  let vidaMonstro = prompt("Digite a vida do Monstro (entre 10 e 50)")
+  let danoJogador = prompt("Digite o Dano do Jogador (entre 5 e 10)")
+  let turnos = Math.ceil(vidaMonstro/danoJogador)
+  console.log(`O jogador irá derrotar o monstro em ${ turnos } turnos`)
+  console.log("__________________________________________")
+
+// Exercicio 7
+  const valorConta = 42.54
+  const taxaServico = valorConta/100
+  console.log(`Valor da taxa de serviço: R$ ${ taxaServico.toFixed(2) }`)
+  console.log("__________________________________________")
+
+// Exercicio 8
+  console.log("__________________________________________")
+
+// Exercicio 1
+  console.log("__________________________________________")
+
