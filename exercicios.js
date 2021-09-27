d0c7f0544660296a8ea38848540e32e0a29e7264
Lima-Murilo <mug_fan@outlook.com>
2021-09-27 20:53:36 -0300

    entrega2

---
+++ exercicios.js
@@ -0,0 +1,34 @@
+//Exercício 1
+const numeroAleatorio = Math.floor(Math.random() * 10 + 1)
+const numeroUsuario = +prompt("Digite um número de 1 a 10")
+
+if(numeroAleatorio === numeroUsuario){
+  console.log("Parabéns, você acertou!")
+}
+else{
+  console.log("Que pena, tente outra vez.")
+}
+
+//Exercício 2
+const numeroUm = 1
+const numeroDois = 2
+
+function func1(argumento1, argumento2){
+  return argumento1 + argumento2
+}
+
+func1(numeroUm, numeroDois)
+
+//Exercício 3
+const numero = 1
+
+function func1(argumento){
+  if (argumento > 0){
+    console.log("P")
+  }
+  else{
+    console.log("R")
+  }
+}
+
+func1(numero)
